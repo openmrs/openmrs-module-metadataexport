18674fe1f8c8e4c8ded0f43d110dbb355afc2af2
Wikum Weerakutti <wikum@linuxdeveloper.space>
2026-06-30 15:25:19 +0530

    ME-3: Support Concept Source Domain
    Add ConceptSourceDomainExporter exporting all concept sources.

---
+++ api/src/main/java/org/openmrs/module/metadataexport/api/concept/ConceptSourceLineExporter.java
@@ -0,0 +1,42 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.metadataexport.api.concept;
+
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.openmrs.ConceptSource;
+import org.openmrs.module.initializer.api.BaseLineProcessor;
+import org.openmrs.module.metadataexport.api.export.BaseLineExporter;
+import org.openmrs.module.metadataexport.api.export.ExportLine;
+
+public class ConceptSourceLineExporter extends BaseLineExporter<ConceptSource> {
+	
+	public static final String HEADER_HL7_CODE = "HL7 Code";
+	
+	public static final String HEADER_UNIQUE_ID = "Unique ID";
+	
+	@Override
+	public void export(ConceptSource conceptSource, ExportLine line) {
+		line.put(BaseLineProcessor.HEADER_UUID, conceptSource.getUuid());
+		if (BooleanUtils.isTrue(conceptSource.getRetired())) {
+			line.put(BaseLineProcessor.HEADER_VOID_RETIRE, "true");
+			return;
+		}
+		line.put(BaseLineProcessor.HEADER_NAME, conceptSource.getName());
+		line.put(BaseLineProcessor.HEADER_DESC, conceptSource.getDescription());
+		
+		if (StringUtils.isNotEmpty(conceptSource.getHl7Code())) {
+			line.put(HEADER_HL7_CODE, conceptSource.getHl7Code());
+		}
+		if (StringUtils.isNotEmpty(conceptSource.getUniqueId())) {
+			line.put(HEADER_UNIQUE_ID, conceptSource.getUniqueId());
+		}
+	}
+}
