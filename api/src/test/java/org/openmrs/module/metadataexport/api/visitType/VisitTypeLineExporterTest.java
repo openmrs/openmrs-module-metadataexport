c26824fdff91358c2fab2ef808fdef40b407c5ea
Bawanthathilan <bawantharathnayaka@gmail.com>
2026-06-30 21:36:48 +0530

    Rename package from 'visittype' to 'visitType' for consistency

--- api/src/test/java/org/openmrs/module/metadataexport/api/visitType/VisitTypeLineExporterTest.java
+++ api/src/test/java/org/openmrs/module/metadataexport/api/visitType/VisitTypeLineExporterTest.java
@@ -7,7 +7,7 @@
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
-package org.openmrs.module.metadataexport.api.visittype;
+package org.openmrs.module.metadataexport.api.visitType;
 
 import org.junit.jupiter.api.Test;
 import org.openmrs.VisitType;
