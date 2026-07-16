ad946520ce452abff018222e08ee9afea858e99e
Wikum Weerakutti <wikum@linuxdeveloper.space>
2026-07-16 12:17:55 +0530

    ME-21: Support OrderType Domain
    - Add `OrderTypeDomainExporter` for exporting order types to CSV.
    - Implement `OrderTypeLineExporter` to process order type-specific fields.
    - Write comprehensive unit tests for `OrderTypeLineExporter` to validate field exports and edge cases.

---
+++ api/src/main/java/org/openmrs/module/metadataexport/api/orderType/OrderTypeDomainExporter.java
@@ -0,0 +1,56 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.metadataexport.api.orderType;
+
+import org.openmrs.OpenmrsObject;
+import org.openmrs.OrderType;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.initializer.Domain;
+import org.openmrs.module.metadataexport.api.export.BaseLineExporter;
+import org.openmrs.module.metadataexport.api.export.CsvDomainExporter;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class OrderTypeDomainExporter extends CsvDomainExporter<OrderType> {
+	
+	@Override
+	protected List<BaseLineExporter<OrderType>> chain() {
+		return Collections.singletonList(new OrderTypeLineExporter());
+	}
+	
+	@Override
+	protected String fileName() {
+		return "orderTypes.csv";
+	}
+	
+	@Override
+	public Domain getDomain() {
+		return Domain.ORDER_TYPES;
+	}
+	
+	@Override
+	public boolean handles(OpenmrsObject instance) {
+		return instance instanceof OrderType;
+	}
+	
+	@Override
+	public Collection<OrderType> getAllInstances() {
+		return Context.getOrderService().getOrderTypes(true);
+	}
+	
+	@Override
+	public Collection<? extends OpenmrsObject> getDependencies(OrderType instance) {
+		return Collections.emptyList();
+	}
+}
