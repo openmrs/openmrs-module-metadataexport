4c4af821a3acb50aa70a1a0449deb41e9c64af5a
Wikum Weerakutti <wikum@linuxdeveloper.space>
2026-09-24 12:14:29 +0530

    Add missing name and description fields to ProgramLineExporter

--- api/src/main/java/org/openmrs/module/metadataexport/domain/program/ProgramLineExporter.java
+++ api/src/main/java/org/openmrs/module/metadataexport/domain/program/ProgramLineExporter.java
@@ -11,6 +11,7 @@ package org.openmrs.module.metadataexport.domain.program;
 
 import org.openmrs.Concept;
 import org.openmrs.Program;
+import org.openmrs.module.initializer.api.BaseLineProcessor;
 import org.openmrs.module.metadataexport.export.ExportLine;
 import org.openmrs.module.metadataexport.export.MetadataLineExporter;
 
@@ -22,6 +23,9 @@ public class ProgramLineExporter extends MetadataLineExporter<Program> {
 	
 	@Override
 	public void export(Program program, ExportLine line) {
+		line.put(BaseLineProcessor.HEADER_NAME, program.getName());
+		line.put(BaseLineProcessor.HEADER_DESC, program.getDescription());
+
 		Concept concept = program.getConcept();
 		if (concept != null) {
 			line.put(HEADER_CONCEPT_PROGRAM, concept.getUuid());
